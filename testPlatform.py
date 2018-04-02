617ccb3f54a494ba3e5f8f0798a7d23716ef0475
JackWeiXH <jackweixh@foxmail.com>
2018-04-03 00:04:29 +0800

    Initial commit

---
+++ testPlatform.py
@@ -0,0 +1,9 @@
+import toolBox as tb
+
+
+def test01():
+    result = tb.match_string("a", "b")
+    print(result)
+
+
+test01()
---
+++ toolBox.py
@@ -0,0 +1,4 @@
+
+
+def match_string(a_str, b_str):
+    return a_str == b_str
