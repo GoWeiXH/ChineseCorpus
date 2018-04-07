2697d675a8ed6f30e71839fc0af845ddf593ce42
JackWeiXH <jackweixh@foxmail.com>
2018-04-07 23:57:31 +0800

    0.0.1.20180407_Base

--- testPlatform.py
+++ testPlatform.py
@@ -1,8 +1,9 @@
-import toolBox as Box
+from toolBox import ToolBox as Box
 
 
 def test01():
     result = Box.match_string("a", "b")
-    print(result)
+    return result
 
 
+test01()
--- toolBox.py
+++ toolBox.py
@@ -1,10 +1,14 @@
 
 
-def match_string(a_str, b_str):
-    """
-    比较两个字符串是否一致
-    :param a_str: 一个字符串（str）
-    :param b_str: 另一个字符串（str）
-    :return: 一致则返回True，不一致则返回False（bool）
-    """
-    return a_str == b_str
+class ToolBox:
+
+    @staticmethod
+    def match_string(a_str, b_str):
+
+        """
+        比较两个字符串是否一致
+        :param a_str: 一个字符串（str）
+        :param b_str: 另一个字符串（str）
+        :return: 一致则返回True，不一致则返回False（bool）
+        """
+        return a_str == b_str
